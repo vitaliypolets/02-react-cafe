75e039270d0e11f43aa444c932c240a8b34d9709
Vitaliy <vspolets@gmail.com>
2026-05-04 22:05:21 +0300

    Fix css import types

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'modern-normalize';
-import './styles.css';
+import 'modern-normalize/modern-normalize.css';
 import App from './components/App/App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
